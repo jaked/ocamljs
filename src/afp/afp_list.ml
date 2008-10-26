4c58c6e59bcd74ffab32b345b5912e8fc4e43a59
Jake Donham <jake@donham.org>
2008-10-26 23:42:32 +0000

    list stuff
    git-svn-id: http://ocamljs.googlecode.com/svn/trunk@64 27578800-e353-0410-a23a-a7c9f63c6ccd

--- src/afp/afp_list.ml
+++ src/afp/afp_list.ml
@@ -1,19 +1,38 @@
-type 'a t = Nil | Cons of 'a * 'a t Afp.t
+type 'a t = Nil | Cons of 'a * 'a at
+and 'a at = 'a t Afp.t
 
-let (>>=) = Afp.(>>=)
+open Afp
 
 let rec filter p l =
   l >>= function
-    | Nil -> Afp.return Nil
+    | Nil -> return Nil
     | Cons (h, t) ->
         let ft = filter p t in
         if p h
-        then Afp.return (Cons (h, ft))
+        then return (Cons (h, ft))
         else ft
+
+let rec partition p l =
+  l >>= function
+    | Nil -> return (return Nil, return Nil)
+    | Cons (h, t) ->
+        partition p t >> fun (yes, no) ->
+          if p h
+          then (return (Cons (h, yes)), no)
+          else (yes, return (Cons (h, no)))
+
+let sort cmp l =
+  let rec qs l sorted =
+    l >>= function
+      | Nil -> return sorted
+      | Cons (h, t) ->
+          partition (fun x -> cmp x h < 0) t >>= fun (lt, ge) ->
+            qs lt (Cons (h, qs ge sorted)) in
+  qs l Nil
 
 let rec to_list l =
   l >>= function
-    | Nil -> Afp.return []
+    | Nil -> return []
     | Cons (h, t) ->
         to_list t >>= fun t ->
-          Afp.return (h::t)
+          return (h::t)
--- src/afp/afp_list.mli
+++ src/afp/afp_list.mli
@@ -1,5 +1,10 @@
-type 'a t = Nil | Cons of 'a * 'a t Afp.t
+type 'a t = Nil | Cons of 'a * 'a at
+and 'a at = 'a t Afp.t
 
-val filter : ('a -> bool) -> 'a t Afp.t -> 'a t Afp.t
+val filter : ('a -> bool) -> 'a at -> 'a at
 
-val to_list : 'a t Afp.t -> 'a list Afp.t
+val partition : ('a -> bool) -> 'a at -> ('a at * 'a at) Afp.t
+
+val sort : ('a -> 'a -> int) -> 'a at -> 'a at
+
+val to_list : 'a at -> 'a list Afp.t
