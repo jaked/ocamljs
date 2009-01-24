ebb621c16edd253b3db6914831e5eff06f8d50a3
Jake Donham <jake@donham.org>
2009-01-24 00:08:48 +0000

    experimental support for recursive modules
    git-svn-id: http://ocamljs.googlecode.com/svn/trunk@114 27578800-e353-0410-a23a-a7c9f63c6ccd

---
+++ src/stdlib/camlinternalMod.ml
@@ -0,0 +1,42 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                           Objective Caml                            *)
+(*                                                                     *)
+(*         Xavier Leroy, projet Cristal, INRIA Rocquencourt            *)
+(*                                                                     *)
+(*  Copyright 2004 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the GNU Library General Public License, with    *)
+(*  the special exception on linking described in file ../LICENSE.     *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* $Id: camlinternalMod.ml,v 1.5.6.2 2007/10/26 15:39:04 xleroy Exp $ *)
+
+type shape =
+  | Function
+  | Lazy
+  | Class
+  | Module of shape array
+
+let rec init_mod loc shape =
+  match shape with
+  | Function ->
+      Obj.repr (fun _ -> raise (Undefined_recursive_module loc))
+  | Lazy ->
+      Obj.repr (lazy (raise (Undefined_recursive_module loc)))
+  | Class ->
+      Obj.repr (CamlinternalOO.dummy_class loc)
+  | Module comps ->
+      Obj.repr (Array.map (init_mod loc) comps)
+
+let rec update_mod shape o n =
+  match shape with
+    | Module comps ->
+        assert (Obj.tag n = 0 && Obj.size n >= Array.length comps);
+        for i = 0 to Array.length comps - 1 do
+          match comps.(i) with
+            | Module _ -> update_mod comps.(i) (Obj.field o i) (Obj.field n i)
+            | _ -> Obj.set_field o i (Obj.field n i)
+        done
+    | _ -> assert false
